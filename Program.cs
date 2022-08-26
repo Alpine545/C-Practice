2cd1d2e9d4cb74cc84ccb9923a9cba4992d6e799
Alpine545 <Alpine545@mail.ru>
2022-08-26 17:03:41 +0300

    Добавили описание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Press array length:");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Press value {i + 1}:");
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    if (array[i].Length <= 3)
+        Console.Write($"{array[i]} ");
+}
